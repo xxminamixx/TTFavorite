a3ba92ba5787d4afa03fe248ff0722828b306588
南　京兵 <k_minami@waku-2.com>
2016-10-06 16:04:03 +0900

    画像表示メソッドをグローバル化し、各メソッドの説明コメント追加

--- TTFavorite/TTFavoriteTableViewCell.h
+++ TTFavorite/TTFavoriteTableViewCell.h
@@ -8,11 +8,23 @@
 
 #import <UIKit/UIKit.h>
 #import "TTFavoriteEntity.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface TTFavoriteTableViewCell : UITableViewCell
 
+// セルに行数制限なしのUILabelをセットし、引数の文字列をこのCellのidとするイニシャライザ
 -(id) initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString*)str;
+
+// Entityを受け取り自身のlabelに名前とテキストをセットする
 - (void)setMyProperty:(TTFavoriteEntity *)entity;
+
+// 自身の高さを返す
 -(CGFloat) height;
+
+// urlの画像を自身のUIImageViewに表示する
+- (void)sd_setImageWithURL:(NSURL *)url completed:(SDWebImageCompletionBlock)completedBlock;
+
+// 画像を再読み込みする
+- (void)imageRefresh:(NSURL *)url;
 
 @end
