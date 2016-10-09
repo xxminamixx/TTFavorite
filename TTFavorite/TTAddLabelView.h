bdfceab751efd1c7917d54b57b8e2f38fec319dc
kyohei.minami <kyohei.minami@sacn.co.jp>
2016-10-09 09:28:41 +0900

    ラベル振り分けようのViewを作成

---
+++ TTFavorite/TTAddLabelView.h
@@ -0,0 +1,13 @@
+//
+//  TTAddLabelView.h
+//  TTFavorite
+//
+//  Created by kyohei.minami on 2016/10/09.
+//  Copyright © 2016年 南　京兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAddLabelView : UIView
+
+@end
---
+++ TTFavorite/TTAddLabelView.m
@@ -0,0 +1,21 @@
+//
+//  TTAddLabelView.m
+//  TTFavorite
+//
+//  Created by kyohei.minami on 2016/10/09.
+//  Copyright © 2016年 南　京兵. All rights reserved.
+//
+
+#import "TTAddLabelView.h"
+
+@implementation TTAddLabelView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
