b68e097585f802636c1eb1320ec49a02e4c022e3
南　京兵 <k_minami@waku-2.com>
2016-10-12 15:42:31 +0900

    新規ラベル用のView追加

---
+++ TTFavorite/TTNewLabelView.m
@@ -0,0 +1,21 @@
+//
+//  TTNewLabelView.m
+//  TTFavorite
+//
+//  Created by 南　京兵 on 2016/10/12.
+//  Copyright © 2016年 南　京兵. All rights reserved.
+//
+
+#import "TTNewLabelView.h"
+
+@implementation TTNewLabelView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
